af3c05897bd3eaa21b3287e1e259d4d1f16433b6
Kavya75340 <kvvarshney7534@gmail.com>
2025-05-03 13:01:44 +0530

    add backend

---
+++ backend/src/main/java/com/hospital/hack4health/Hack4healthApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.hack4health;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hack4healthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hack4healthApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hospital/hack4health/Hack4healthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.hack4health;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hack4healthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
